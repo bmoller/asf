7b76a23e556d1e22865d003104e8417a250a5efe
Brandon Moller <brandon@bkmoller.net>
2017-06-29 13:58:22 +0000

    Cleaned up Card tests to match the rest of tests

--- tests/test_alexa_response_module.py
+++ tests/test_alexa_response_module.py
@@ -1,165 +1,157 @@
 import unittest
 
-import alexa.card
+import alexa.response
 
 
 class AlexaCardTestCase(unittest.TestCase):
-    """
-
-    """
 
     def setUp(self):
-        """ Set some simple attributes used in multiple places
-        """
+        """Set some simple attributes used in multiple places"""
 
         self.card_text = 'This is some sweet card content'
         self.card_title = 'This is a sweet card title'
         self.image_url = 'https://coolstuff.com/one/sweet/image.jpg'
-        self.bad_types = [
-            123,
-            (),
-            [],
-            {},
-        ]
-        self.bad_urls = {
-            'missing_scheme': 'i/dont/have/a/scheme',
-            'missing_netloc': 'https:///i/dont/have/a/netloc',
-            'bad_scheme': 'this://is/a/bad/scheme',
-            'bad_netloc': 'https://this/is/a/bad/netloc',
-            'http_scheme': 'http://this.isnt/a/ssl/url',
-            'not_jpeg_or_png': 'https://this.image/isnt/a/valid/type.gif',
-        }.values()
+
+        self.type_check_test_cases = {
+            'int Input': 123,
+            'tuple Input': (),
+            'list Input': [],
+            'dict Input': {},
+        }
+
+        self.url_check_test_cases = {
+            'HTTP Scheme': 'http://this.isnt/a/ssl/url',
+            'Invalid Image Type': 'https://this.image/isnt/a/valid/type.gif',
+            'Invalid netloc': 'https://this/is/a/bad/netloc',
+            'Invalid Scheme': 'this://is/a/bad/scheme',
+            'Missing netloc': 'https:///i/dont/have/a/netloc',
+            'Missing Scheme': 'i/dont/have/a/scheme',
+        }
+
+        self.card_creation_test_cases = {
+            'Simple Card': {
+                'input': {
+                    'args': [
+                        self.card_text,
+                    ],
+                    'kwargs': {},
+                },
+                'string': {
+                    'content': self.card_text,
+                    'type': 'Simple',
+                }
+            },
+            'Simple Card with Title': {
+                'input': {
+                    'args': [
+                        self.card_text,
+                    ],
+                    'kwargs': {
+                        'title': self.card_title,
+                    },
+                },
+                'string': {
+                    'content': self.card_text,
+                    'title': self.card_title,
+                    'type': 'Simple',
+                },
+            }
+        }
 
     def tearDown(self):
-        """ Remove the attributes created in test setup
-        """
+        """Remove the attributes created in test setup"""
 
-        for attribute in ['card_text', 'card_title', 'image_url', 'bad_types',
-                          'bad_urls', ]:
+        for attribute in [
+                'card_text', 'card_title', 'image_url', 'url_check_test_cases',
+                'type_check_test_cases', 'card_creation_test_cases', ]:
             delattr(self, attribute)
 
-    def test_simple_card(self):
-        """ Check that the simple card type returns correctly
-        """
+    def test_card_creation(self):
+        """Check that the simple card type returns correctly"""
 
-        expected_card_output = {
-            'type': 'Simple',
-            'content': self.card_text,
-        }
-        card_output = alexa.card.build_card(self.card_text)
+        for test_case, test_parameters in self.card_creation_test_cases.items():
+            test_input = test_parameters['input']
+            test_card = alexa.response.Card(
+                *test_input['args'], **test_input['kwargs'])
 
-        self.assertEqual(card_output, expected_card_output)
+            with self.subTest(test_case=test_case):
+                self.assertEqual(
+                    test_card.text, test_input['args'][0],
+                    msg='New Card has incorrect text')
 
-    def test_simple_card_with_title(self):
-        """ Check that the simple card type returns correctly with a title
-        """
+            if 'title' in test_input['kwargs']:
+                with self.subTest(test_case=test_case):
+                    self.assertEqual(
+                        test_card.title, test_input['kwargs']['title'],
+                        msg='New Card has an incorrect title')
 
-        expected_card_output = {
-            'type': 'Simple',
-            'title': self.card_title,
-            'content': self.card_text,
-        }
-        card_output = alexa.card.build_card(self.card_text,
-                                            title=self.card_title)
+            if 'small_image' in test_input['kwargs']:
+                with self.subTest(test_case=test_case):
+                    self.assertEqual(
+                        test_card.small_image,
+                        test_input['kwargs']['small_image'],
+                        msg='New Card has an incorrect small_image URL')
 
-        self.assertEqual(card_output, expected_card_output)
-
-    def test_standard_card_with_images(self):
-        """ Check that the standard card type returns correctly with images
-        """
-
-        for image_type in [('small_image', 'smallImageUrl'),
-                           ('large_image', 'largeImageUrl'), ]:
-
-            with self.subTest(image_type=image_type):
-                expected_card_output = {
-                    'type': 'Standard',
-                    'text': self.card_text,
-                    'image': {
-                        image_type[1]: self.image_url,
-                    },
-                }
-                image_parameter = {
-                    image_type[0]: self.image_url,
-                }
-                card_output = alexa.card.build_card(self.card_text,
-                                                    **image_parameter)
-
-                self.assertEqual(card_output, expected_card_output)
-
-        with self.subTest():
-            expected_card_output = {
-                'type': 'Standard',
-                'text': self.card_text,
-                'image': {
-                    'smallImageUrl': self.image_url,
-                    'largeImageUrl': self.image_url,
-                },
-            }
-            card_output = alexa.card.build_card(self.card_text,
-                                                small_image=self.image_url,
-                                                large_image=self.image_url)
-
-            self.assertEqual(card_output, expected_card_output)
-
-        with self.subTest():
-            expected_card_output = {
-                'type': 'Standard',
-                'text': self.card_text,
-                'title': self.card_title,
-                'image': {
-                    'smallImageUrl': self.image_url,
-                    'largeImageUrl': self.image_url,
-                },
-            }
-            card_output = alexa.card.build_card(self.card_text,
-                                                title=self.card_title,
-                                                small_image=self.image_url,
-                                                large_image=self.image_url)
-
-            self.assertEqual(card_output, expected_card_output)
+            if 'large_image' in test_input['kwargs']:
+                with self.subTest(test_case=test_case):
+                    self.assertEqual(
+                        test_card.large_image,
+                        test_input['kwargs']['large_image'],
+                        msg='New Card has an incorrect large_image URL')
 
     def test_nonstring_parameters_raise_exceptions(self):
         """ Check that nonstring parameters to card creation raise exceptions
         """
 
-        for bad_type in self.bad_types:
+        for test_case, test_input in self.type_check_test_cases.items():
 
-            with self.subTest(card_text=bad_type):
-                with self.assertRaises(TypeError):
-                    alexa.card.build_card(bad_type)
+            with self.subTest(card_text=test_case):
+                with self.assertRaises(
+                        TypeError,
+                        msg='Non-string card text doesn\'t raise an exception'):
+                    alexa.response.Card(test_input)
 
-            with self.subTest(card_title=bad_type):
-                with self.assertRaises(TypeError):
-                    alexa.card.build_card(self.card_text, title=bad_type)
+            with self.subTest(card_title=test_case):
+                with self.assertRaises(
+                        TypeError,
+                        msg='Non-string card title doesn\'t raise an exception'):
+                    alexa.response.Card(self.card_text, title=test_input)
 
-            with self.subTest(small_image=bad_type):
-                with self.assertRaises(TypeError):
-                    alexa.card.build_card(self.card_text, small_image=bad_type)
+            with self.subTest(small_image=test_case):
+                with self.assertRaises(
+                        TypeError,
+                        msg='Non-string small image URL doesn\'t raise an exception'):
+                    alexa.response.Card(self.card_text, small_image=test_input)
 
-            with self.subTest(large_image=bad_type):
-                with self.assertRaises(TypeError):
-                    alexa.card.build_card(self.card_text, large_image=bad_type)
+            with self.subTest(large_image=test_case):
+                with self.assertRaises(
+                        TypeError,
+                        msg='Non-string large image URL doesn\'t raise an exception'):
+                    alexa.response.Card(self.card_text, large_image=test_input)
 
-    def test_bad_image_urls_raise_an_exception(self):
-        """ Check that various URL's of invalid format raise exceptions
-        """
+    def test_bad_image_urls_raise_an_exception_at_card_creation(self):
+        """Check that various URL's of invalid format raise exceptions"""
 
-        for bad_url in self.bad_urls:
+        for test_case, test_input in self.url_check_test_cases.items():
 
-            with self.subTest(small_image_url=bad_url):
-                with self.assertRaises(ValueError):
-                    alexa.card.build_card(self.card_text, small_image=bad_url)
+            with self.subTest(small_image=test_case):
+                with self.assertRaises(
+                        ValueError,
+                        msg='Invalid small image URL doesn\'t raise an exception'):
+                    alexa.response.Card(self.card_text, small_image=test_input)
 
-            with self.subTest(large_image_url=bad_url):
-                with self.assertRaises(ValueError):
-                    alexa.card.build_card(self.card_text, large_image=bad_url)
+            with self.subTest(large_image=test_case):
+                with self.assertRaises(
+                        ValueError,
+                        msg='Invalid small image URL doesn\'t raise an exception'):
+                    alexa.response.Card(self.card_text, large_image=test_input)
 
     def test_check_image_url_method(self):
-        """ Check that the image URL check method raises exceptions when appropriate
-        """
+        """ Check image URL verification raises exceptions when appropriate"""
 
-        for bad_url in self.bad_urls:
-            with self.subTest(url=bad_url):
-                with self.assertRaises(ValueError):
-                    alexa.card._check_image_url(bad_url)
+        for test_case, test_input in self.url_check_test_cases.items():
+            with self.subTest(test_case=test_case):
+                with self.assertRaises(
+                        ValueError,
+                        msg='No exception was raised for a nonstring image URL'):
+                    alexa.response.Card._check_image_url(test_input)
